796ac2349e886ae96df2653f5ebbe2d79da8a0e7
村松龍之介 <riscait@imac-mr.local>
2017-04-18 17:06:05 +0900

    BGM, SE, 猫アイコン等を追加

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -13,24 +13,41 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var scrollNode: SKNode!
     var wallNode: SKNode!
     var bird: SKSpriteNode!
+    var fish: SKSpriteNode!
+    
     
     // 衝突判定カテゴリー
     let birdCategory: UInt32 = 1 << 0       // 00000000000000000000000000000001
     let groundCategory: UInt32 = 1 << 1     // 00000000000000000000000000000010
     let wallCategory: UInt32 = 1 << 2       // 00000000000000000000000000000100
     let scoreCategory: UInt32 = 1 << 3      // 00000000000000000000000000001000
+    let fishCategory: UInt32 = 1 << 4      // 00000000000000000000000000010000
+    
     
     // スコア用
     var score = 0
     var scoreLabelNode: SKLabelNode!
     var bestScoreLabelNode: SKLabelNode!
+    var itemScore = 0
+    var itemScoreLabelNode: SKLabelNode!
+    
     let userDefaults: UserDefaults = UserDefaults.standard
     
+    // BGM
+    let bgm = SKAction.playSoundFileNamed("ゆかいな日常.mp3", waitForCompletion: true)
+    
+    // 効果音
+    let meow = SKAction.playSoundFileNamed("猫01.mp3", waitForCompletion: true)
+    let meow2 = SKAction.playSoundFileNamed("にゃあ.mp3", waitForCompletion: true)
     
     // SKView上にシーンが表示された時に呼ばれるメソッド
     override func didMove(to view: SKView) {
+        // BGMをループして再生
+        let loopBgm = SKAction.repeatForever(bgm)
+        self.run(loopBgm)
+        
         // 重力を設定
-        physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
+        physicsWorld.gravity = CGVector(dx: 0.0, dy: -3.0)
         physicsWorld.contactDelegate = self
         
         // 背景色を設定
@@ -44,12 +61,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         wallNode = SKNode()
         scrollNode.addChild(wallNode)
         
+        // 秋刀魚用のノード
+        fish = SKSpriteNode()
+        scrollNode.addChild(fish)
+        
         // 各種スプライトを生成する処理をメソッドに分割
         setupGround()
         setupCloud()
         setupWall()
         setupBird()
-        
+        setupFish()
         setupScoreLabel()
     }
     
@@ -62,7 +83,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         if (contact.bodyA.categoryBitMask & scoreCategory) == scoreCategory || (contact.bodyB.categoryBitMask & scoreCategory) == scoreCategory {
             // スコア用の物体と衝突した
-            print("ScoreUP")
+            print("ScoreUP!")
             score += 1
             scoreLabelNode.text = "Score:\(score)"
             
@@ -73,10 +94,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 bestScoreLabelNode.text = "Best Score:\(bestScore)"
                 userDefaults.set(bestScore, forKey: "BEST")
                 userDefaults.synchronize()
+                print("BestScore!!")
             }
+        } else if (contact.bodyA.categoryBitMask & fishCategory) == fishCategory || (contact.bodyB.categoryBitMask & fishCategory) == fishCategory {
+            // アイテムと接触した
+            print("ItemGet!")
+            itemScore += 1
+            itemScoreLabelNode.text = "Item:\(itemScore)"
+            self.run(meow2)
+            fish.removeAllChildren()
         } else {
             // 地面と衝突した
-            print("GameOver")
+            print("GameOver!")
+            self.run(meow)
             
             // スクロールを停止させる
             scrollNode.speed = 0
@@ -97,7 +127,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             bird.physicsBody?.velocity = CGVector.zero
         
             // 鳥に縦方向の力を与える
-            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 25))
+            bird.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 35))
         } else if bird.speed == 0 {
             restart()
         }
@@ -106,6 +136,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func restart() {
         score = 0
         scoreLabelNode.text = "Score:\(score)"
+        itemScore = 0
+        itemScoreLabelNode.text = "Item:\(itemScore)"
         
         bird.position = CGPoint(x: self.frame.size.width * 0.2, y: self.frame.size.height * 0.7)
         bird.physicsBody?.velocity = CGVector.zero
@@ -113,6 +145,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bird.zRotation = 0.0
         
         wallNode.removeAllChildren()
+        fish.removeAllChildren()
         
         bird.speed = 1
         scrollNode.speed = 1
@@ -236,7 +269,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let under_wall_y = CGFloat(under_wall_lowest_y + random_y)
             
             // キャラが通り抜ける隙間の長さ
-            let slit_length = self.frame.size.height / 5
+            let slit_length = self.frame.size.height / 4
             
             // 下側の壁を作成
             let under = SKSpriteNode(texture: wallTexture)
@@ -262,6 +295,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             // 衝突の時に動かないように設定
             upper.physicsBody?.isDynamic = false
             
+            // スプライトにアクションを設定する
             wall.run(wallAnimation)
             
             // スコアに加点するためのノード
@@ -301,7 +335,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bird = SKSpriteNode(texture: birdTextureA)
         bird.position = CGPoint(x: self.frame.size.width * 0.2, y: self.frame.size.height * 0.7)
         
-        //物理演算を設定
+        // 物理演算を設定
         bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2.0)
         
         // 衝突した時に回転させない
@@ -319,6 +353,71 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(bird)
     }
     
+    func setupFish() {
+        // 秋刀魚の画像を読み込む
+        let fishTexture = SKTexture(imageNamed: "Fish")
+        fishTexture.filteringMode = SKTextureFilteringMode.linear // 当たり判定を行うので画質優先
+        
+        // 移動する距離を計算
+        let movingDistance = CGFloat(self.frame.size.width + fishTexture.size().width * 4)
+        
+        // 画面外まで移動するアクションを作成
+        let moveFish = SKAction.moveBy(x: -movingDistance, y: 0, duration: 5.2)
+        
+        // 自身を取り除くアクションを作成
+        let removeFish = SKAction.removeFromParent()
+        
+        // 2つのアニメーションを順に実行するアクションを作成
+        let fishAnimation = SKAction.sequence([moveFish, removeFish])
+        
+        // 秋刀魚を生成するアクションを作成
+        let createFishAnimation = SKAction.run ({
+            // 画面のY軸の中央値
+            let center_y = self.frame.size.height / 2
+            
+            // 秋刀魚のY座標を上下ランダムにさせるときの最大値
+            let random_y_range = self.frame.size.height / 8
+            
+            // 秋刀魚のY軸の下限
+            let fish_lowest_y = UInt32(center_y - fishTexture.size().height / 2 - random_y_range / 2)
+            
+            // 1〜random_y_rangeまでのランダムな整数を生成
+            let random_y = arc4random_uniform(UInt32(random_y_range))
+            
+            // Y軸の下限にランダムな値を足して、秋刀魚のY座標を決定
+            let fish_y = CGFloat(fish_lowest_y + random_y)
+            
+            // 秋刀魚のスプライトを作成
+            let fish = SKSpriteNode(texture: fishTexture)
+            fish.position = CGPoint(x: self.frame.size.width + fishTexture.size().width * 3, y: fish_y)
+            fish.zPosition = -20
+            
+            // スプライトに物理演算を設定
+            fish.physicsBody = SKPhysicsBody(rectangleOf: fishTexture.size())
+            fish.physicsBody?.categoryBitMask = self.fishCategory
+            fish.physicsBody?.contactTestBitMask = self.birdCategory
+            
+            // 衝突の時に動かないよう設定
+            fish.physicsBody?.isDynamic = false
+            
+            // スプライトにアクションを設定する
+            fish.run(fishAnimation)
+            
+            self.fish.addChild(fish)
+        })
+        
+        // 次の秋刀魚作成までの待ち時間のアクションを作成
+        let waitAnimation = SKAction.wait(forDuration: 8.0)
+        
+        // 秋刀魚を作成 -> 待ち時間 -> 秋刀魚を作成を無限に繰り替えるアクションを作成
+        let repeatForeverAnimation = SKAction.repeatForever(SKAction.sequence([createFishAnimation, waitAnimation]))
+        
+        // スプライトにアクションを設定する
+        fish.run(repeatForeverAnimation)
+
+    }
+
+    
     func setupScoreLabel() {
         score = 0
         scoreLabelNode = SKLabelNode()
@@ -334,9 +433,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         bestScoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 60)
         bestScoreLabelNode.zPosition = 100
         bestScoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
-        
         let bestScore = userDefaults.integer(forKey: "BEST")
         bestScoreLabelNode.text = "Best Score:\(bestScore)"
         self.addChild(bestScoreLabelNode)
+
+        itemScore = 0
+        itemScoreLabelNode = SKLabelNode()
+        itemScoreLabelNode.fontColor = UIColor.black
+        itemScoreLabelNode.position = CGPoint(x: 10, y: self.frame.size.height - 90)
+        itemScoreLabelNode.zPosition = 100
+        itemScoreLabelNode.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        itemScoreLabelNode.text = "Item:\(itemScore)"
+        self.addChild(itemScoreLabelNode)
     }
 }
